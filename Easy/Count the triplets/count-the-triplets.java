e2d20ab754deec361ff0c86fa840a8e7440cc3bf
Saurabh Saxena <saurabh.jan03@gmail.com>
2023-12-15 00:59:29 -0500

    Added Solution - GfG to GitHub

---
+++ Easy/Count the triplets/count-the-triplets.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            int n = Integer.parseInt(read.readLine());
+            String input[] = read.readLine().split(" ");
+            
+            int arr[] = new int[n];
+            for(int i = 0;i<n;i++){
+                arr[i] = Integer.parseInt(input[i]);
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countTriplet(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countTriplet(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n ; i++){
+            map.put(arr[i], 1);
+        }
+        int count = 0;
+        for(int i = 0; i < n ; i++){
+            for(int j = i+1; j < n ; j++){
+                int result = 0;
+                result = arr[i] + arr[j];
+                if(map.containsKey(result)){
+                    count++;
+                }
+            }
+         
+        }
+        return count;
+    }
+}
