bd5d055d39385fb6cae5eb04d8726b83b69adbab
Saurabh Saxena <saurabh.jan03@gmail.com>
2025-08-23 20:43:33 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -0,0 +1,33 @@
+// User function Template for Java
+
+class Solution {
+    // Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String s1, String s2) {
+        // Your code here
+        int m = s1. length();
+        int n = s2. length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return m + n - lcs(s1, s2, m, n, dp);
+    }
+    
+    public static int lcs(String s1, String s2, int m, int n, int[][] dp){
+        if(m == 0 || n == 0){
+            return 0;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        if(s1.charAt(m-1) == s2.charAt(n-1)){
+            dp[m][n] = 1 + lcs(s1, s2, m-1, n-1, dp);
+        }
+        else{
+            dp[m][n] = Math.max(lcs(s1, s2, m-1, n, dp), lcs(s1, s2, m, n-1, dp));
+        }
+        return dp[m][n];
+    }
+}                                                                      
