628370595711cb1ca0810f9420a617298593b2c6
Saurabh Saxena <saurabh.jan03@gmail.com>
2025-11-26 02:06:22 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int celebrity(int mat[][]) {
+        int n = mat.length;
+        int left = 0;
+        int right = n-1;
+        
+        while(left < right){
+            if(mat[left][right] == 1) left++;
+            else right --;
+        }
+        int cand = left;
+        //System.out.println(right);
+        for(int i = 0; i < n; i++){
+            if(i == cand) continue;
+            if(mat[left][i] != 0) return -1;
+        }
+        for(int i = 0; i < n; i++){
+            if(i == cand) continue;
+            if(mat[i][right] != 1) return -1;
+        }
+        return left;
+    }
+}
