135b557519b49e84fc1211d5ad3b9d476f48ddb7
Saurabh Saxena <saurabh.jan03@gmail.com>
2025-12-23 21:08:50 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.java
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.java
@@ -14,11 +14,14 @@ class Node {
 class Solution {
     public int height(Node root) {
         // code here
-        if(root == null){
-            return -1;
-        }
-        int left = height(root.left);
-        int right = height(root.right);
-        return 1 + Math.max(right, left);
+        return helper(root)-1;
+        
+    }
+    
+    public int helper(Node root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        return 1 + Math.max(left, right);
     }
 }
