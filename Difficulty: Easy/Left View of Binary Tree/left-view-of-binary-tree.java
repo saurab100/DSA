3071638e0fa418aba00b45d0fdec28b9c45b4aca
Saurabh Saxena <saurabh.jan03@gmail.com>
2025-12-25 00:31:36 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,31 +1,33 @@
 /*
-class Node
-{
+class Node {
     int data;
     Node left, right;
 
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
     }
-}*/
+}
+*/
+
 class Solution {
-    ArrayList<Integer> leftView(Node root) {
+    public ArrayList<Integer> leftView(Node root) {
         // code here
-        ArrayList<Integer> result = new ArrayList<>();
-        helper(root, 0, result);
-        return result;
-    }
-    
-    public void helper(Node root, int newLevel, ArrayList<Integer> result){
-        if(root == null){
-            return;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Queue<Node> q = new LinkedList<>();
+        if(root == null) return list;
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i  < size; i++){
+                Node node = q.poll();
+                if(i == 0) list.add(node.data);
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
         }
-        if(result.size() == newLevel){
-            result.add(root.data);
-        }
-        helper(root.left, newLevel + 1, result);
-        helper(root.right, newLevel + 1, result);
+        return list;
     }
 }
