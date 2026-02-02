e2d3a290f547fa3bbfa71c5e2961d80268749ab5
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-02 00:04:01 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -1,23 +1,15 @@
 class Solution {
-    // Function to find equilibrium point in the array.
     public static int findEquilibrium(int arr[]) {
         // code here
         int n = arr.length;
-        int[] prefixSum = new int[n];
-        int result = 0;
-        prefixSum[0] = arr[0];
-        for(int i = 1; i < n ; i++){
-            prefixSum[i] = arr[i] + prefixSum[i-1];
-        }
-        int leftSum = 0;
-        int rightSum = 0;
-        for(int i = 1; i < n; i++){
-            
-            leftSum = prefixSum[i];
-            rightSum = prefixSum[n-1] - prefixSum[i-1];
-            if(leftSum == rightSum){
-                return i;
-            }
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+        for(int i = 1; i < n; i++) prefix[i] += prefix[i-1] + arr[i];
+        //System.out.println(Arrays.toString(prefix));
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                if(prefix[i] == prefix[n-1]) return i;}
+            else if(prefix[i] == prefix[n-1]-prefix[i-1]) return i;
         }
         return -1;
     }
