d2bc905fd4252906caaac27650ade8943b912c6d
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-14 23:46:36 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
+++ Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
@@ -2,12 +2,13 @@ class Solution {
     public int longestUniqueSubstr(String s) {
         // code here
         Map<Character, Integer> map = new HashMap<>();
-        int longestUniqueSubstring = 0;
-        int i = 0; int j = 0;
-        while(j < s.length()){
+        int i = 0;
+        int maxLen = 0;
+        
+        for(int j = 0; j < s.length(); j++){
             if(map.containsKey(s.charAt(j))) map.put(s.charAt(j), map.get(s.charAt(j))+1);
             else map.put(s.charAt(j), 1);
-            if(map.size() == j-i+1) longestUniqueSubstring = Math.max(longestUniqueSubstring, j-i+1);
+            if(map.size() >= j-i+1) maxLen = Math.max(maxLen, j - i + 1);
             else if(map.size() < j-i+1){
                 while(map.size() < j-i+1){
                     map.put(s.charAt(i), map.get(s.charAt(i))-1);
@@ -15,8 +16,7 @@ class Solution {
                     i++;
                 }
             }
-            j++;
         }
-        return longestUniqueSubstring;
+        return maxLen;
     }
 }
