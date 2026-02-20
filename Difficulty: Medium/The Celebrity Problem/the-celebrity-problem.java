2f4525498f3d66b5e0a03dbd8e142c3e637dbb1d
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-20 01:01:08 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -1,21 +1,23 @@
 class Solution {
     public int celebrity(int mat[][]) {
+        
         int n = mat.length;
-        int left = 0;
-        int right = n-1;
+        int candidate = 0;
         
-        while(left < right){
-            if(mat[left][right] == 1) left++;
-            else right--;
-        }
-        int cand = left;
-        for(int i = 0; i < n; i++){
-            if(mat[cand][i] != 0 && cand != i) return -1;
+        for(int i = 1; i < n; i++){
+            if(knows(mat, candidate, i)) candidate = i;
         }
         
         for(int i = 0; i < n; i++){
-            if(mat[i][cand] != 1 && cand != i) return -1;
+            if(candidate != i && mat[candidate][i] != 0) return -1;
+            else if(candidate != i && mat[i][candidate] != 1) return -1;
         }
-        return cand;
+        return candidate;
+        
+    }
+    
+    boolean knows(int mat[][], int a, int b){
+        if(mat[a][b] == 1) return true;
+        else return false;
     }
 }
