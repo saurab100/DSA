ab7ff9d91f5cf68464a0087dfde6e8d49b6dee5a
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-21 16:18:13 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
@@ -1,53 +1,45 @@
 class Solution {
     public ArrayList<String> ratInMaze(int[][] maze) {
-        // code here
-        ArrayList<String> output = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<String>();
         int n = maze.length;
         boolean[][] visited = new boolean[n][n];
         StringBuilder str = new StringBuilder();
-        helper(maze, 0, 0, output, visited, n, str);
-        return output;
-        
+        backtrack(maze, 0, 0, n, str, visited, result);
+        return result;
     }
     
-    public void helper(int[][] maze, int row, int col, ArrayList<String> output, boolean[][] visited, int n, StringBuilder str){
+    public void backtrack(int[][] maze, int row, int col,  int n, StringBuilder str, boolean[][] visited, ArrayList<String> result){
         if(row == n-1 && col == n-1){
-            output.add(str.toString());
+            result.add(str.toString());
             return;
         }
-        if(col > n-1 || row < 0 || row >= n || col < 0 || maze[row][col] == 0 || visited[row][col] == true) return;
+        if(row < 0 || row >= n || col < 0 || col >= n) return;
+        if(maze[row][col] == 0 || visited[row][col]) return;
+        
         visited[row][col] = true;
+       
+        //Down
+        str.append("D");
+        backtrack(maze, row+1, col,  n, str, visited, result);
+        str.deleteCharAt(str.length()-1);
         
+         //left
+        str.append("L");
+        backtrack(maze, row, col-1,  n, str, visited, result);
+        str.deleteCharAt(str.length()-1);
         
+         //Right
+        str.append("R");
+        backtrack(maze, row, col + 1,  n, str, visited, result);
+        str.deleteCharAt(str.length()-1);
         
-         //DOWN
-        if(row+1 < n && maze[row+1][col] == 1 && !visited[row+1][col]){
-            str.append("D");
-            helper(maze, row+1, col, output, visited, n, str);
-            str.deleteCharAt(str.length()-1);
-        }
-        
-         //LEFT
-        if(col-1 >= 0 && maze[row][col-1] == 1 && !visited[row][col-1]){
-            str.append("L");
-            helper(maze, row, col-1, output, visited, n, str);
-            str.deleteCharAt(str.length()-1);
-        }
-        
-         //RIGHT
-        if(col+1 < n && maze[row][col+1] == 1 && !visited[row][col+1]){
-            str.append("R");
-            helper(maze, row, col+1, output, visited, n, str);
-            str.deleteCharAt(str.length()-1);
-        }
-        
-        //UP
-        if(row-1 >= 0 && maze[row-1][col] == 1 && !visited[row-1][col]){
-            str.append("U");
-            helper(maze, row-1, col, output, visited, n, str);
-            str.deleteCharAt(str.length()-1);
-        }
+         //Up
+        str.append("U");
+        backtrack(maze, row-1, col,  n, str, visited, result);
+        str.deleteCharAt(str.length()-1);
         
         visited[row][col] = false;
+        
+        
     }
 }
