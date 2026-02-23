4d1f546f4fd01570e7bf4083ee332ac6cf637b46
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-22 21:24:00 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -11,38 +11,38 @@ class Node {
     }
 }
 */
-class Pair {
+class Pair{
     Node node;
     int x;
-
+    
     Pair(Node node, int x){
-        this.node = node;
         this.x = x;
+        this.node = node;
     }
 }
-
 class Solution {
     public ArrayList<Integer> bottomView(Node root) {
         // code here
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Pair> q = new ArrayDeque<>();
+        if(root == null) return new ArrayList<>();
+        q.offer(new Pair(root, 0));
         ArrayList<Integer> list = new ArrayList<Integer>();
-        int x = 0;
-        if (root == null) return list;
-        q.add(new Pair(root, x));
-        Map<Integer, Node> map = new TreeMap<>();
-        while (!q.isEmpty()) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        while(!q.isEmpty()){
+            
             int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Pair p1 = q.poll();
-                Node node = p1.node;
-                x = p1.x;
-                map.put(x, node);
-                if(node.left != null) q.add(new Pair(node.left, x-1));
-                if(node.right != null) q.add(new Pair(node.right, x+1));
+            for(int i = 0; i < size; i++){
+                Pair pair = q.poll();
+                Node node = pair.node;
+                int x = pair.x;
+                map.put(x, node.data);
+                
+                if(node.left != null) q.offer(new Pair(node.left, x-1));
+                if(node.right != null) q.offer(new Pair(node.right, x+1));
             }
         }
-        for(Map.Entry<Integer, Node> m : map.entrySet()){
-            list.add(m.getValue().data);
+        for(Map.Entry<Integer, Integer> m : map.entrySet()){
+            list.add(m.getValue());
         }
         return list;
     }
