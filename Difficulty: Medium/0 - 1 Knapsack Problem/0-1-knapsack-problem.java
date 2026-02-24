e379f12b90e9ce6bdf84fe01210830deb1538163
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-24 00:17:52 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,29 +1,23 @@
 class Solution {
-    static int knapsack(int W, int val[], int wt[]) {
-        int n = wt.length;
+    public int knapsack(int W, int val[], int wt[]) {
+        // code here
+        //base case
+        int n = val.length;
         int[][] dp = new int[n+1][W+1];
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= W; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(W, val, wt, n, dp);
+        for(int[] row : dp) Arrays.fill(row, -1);
+        int result = helper(W, val, wt, n, dp);
+        return result;
     }
-        static int helper(int W, int val[], int wt[], int n, int[][] dp) {
-        if( n == 0 || W == 0){
+    
+    public int helper(int W, int val[], int wt[], int n, int[][] dp){
+        if(n == 0 || W == 0) {
             return 0;
         }
-        if(dp[n][W] != -1){
-            return dp[n][W]; 
+        if(dp[n][W] != -1) return dp[n][W];
+        else if(wt[n-1] <= W){
+             dp[n][W] = Math.max(val[n-1] + helper(W - wt[n-1], val, wt, n-1, dp), helper(W, val, wt, n-1, dp));
         }
-        if( wt[n-1] <= W){
-            dp[n][W] = Math.max(val[n-1]+helper(W -wt[n-1], val, wt, n-1, dp ), helper(W, val, wt, n-1, dp)) ;
-            
-        }
-        else{
-            dp[n][W] = helper(W, val, wt, n-1, dp);
-        }
+        else dp[n][W] = helper(W, val, wt, n-1, dp);
         return dp[n][W];
-        }
-
+    }
 }
