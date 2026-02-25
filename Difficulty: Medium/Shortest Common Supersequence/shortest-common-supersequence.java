a8465761f026863fd10e4ec218caad16892cfeb5
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-25 01:01:16 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -1,33 +1,21 @@
-// User function Template for Java
-
 class Solution {
-    // Function to find length of shortest common supersequence of two strings.
-    public static int shortestCommonSupersequence(String s1, String s2) {
-        // Your code here
-        int m = s1. length();
-        int n = s2. length();
-        int[][] dp = new int[m+1][n+1];
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return m + n - lcs(s1, s2, m, n, dp);
+    static int[][] dp;
+    public static int minSuperSeq(String s1, String s2) {
+        // code here
+        int m = s1.length();
+        int n = s2.length();
+        dp = new int[m+1][n+1];
+        for(int[] d : dp) Arrays.fill(d, -1);
+        return m+n-helper(s1, s2, m, n);
     }
     
-    public static int lcs(String s1, String s2, int m, int n, int[][] dp){
-        if(m == 0 || n == 0){
-            return 0;
+    static int helper(String s1, String s2, int m, int n){
+        if(m == 0 || n == 0) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        else if(s1.charAt(m-1) == s2.charAt(n-1)){
+            dp[m][n] = 1 + helper(s1, s2, m-1, n-1);
         }
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-        if(s1.charAt(m-1) == s2.charAt(n-1)){
-            dp[m][n] = 1 + lcs(s1, s2, m-1, n-1, dp);
-        }
-        else{
-            dp[m][n] = Math.max(lcs(s1, s2, m-1, n, dp), lcs(s1, s2, m, n-1, dp));
-        }
+        else dp[m][n] = Math.max(helper(s1, s2, m-1, n), helper(s1, s2, m, n-1));
         return dp[m][n];
     }
-}                                                                      
+}
