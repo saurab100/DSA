b215d7c4e034fc9d4ee840180ebb8debfe263c0f
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-24 19:50:54 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,35 +1,19 @@
 class Solution {
-
+    static Boolean[][] dp;
     static Boolean isSubsetSum(int arr[], int sum) {
-        // code here
-        int[][] dp = new int[arr.length+1][sum+1];
-        for(int i = 0; i <= arr.length; i++){
-            for(int j = 0; j <= sum; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(arr, sum, arr.length, dp);
-    }
-    
-    static Boolean helper(int arr[], int sum, int n, int[][] dp){
+        int n = arr.length;
+        dp = new Boolean[n+1][sum+1];
+        return helper( arr,sum, n);
         
-        if(sum == 0){
-            return true;
+    }
+    static Boolean helper(int arr[], int sum, int n){
+        if(sum == 0) return true;
+        if(n == 0 && sum > 0) return false;
+        if(dp[n][sum] != null) return dp[n][sum];
+        else if(arr[n-1] <= sum){
+            dp[n][sum] =  helper(arr, sum -arr[n-1], n-1) || helper(arr, sum, n-1);
         }
-        if(n == 0 ){;
-            return false;
-        }
-        if(dp[n][sum] != -1){
-            return dp[n][sum] == 1;
-        }
-        boolean result;
-        if(arr[n-1] <= sum){
-             result = (helper(arr, sum-arr[n-1], n-1, dp)  || helper(arr, sum, n-1, dp)) ;
-        }
-        else{
-             result = helper(arr, sum, n-1, dp);
-        }
-        dp[n][sum] = result ? 1 : 0;
-        return result;
-    } 
+        else dp[n][sum] = helper(arr, sum, n-1);
+        return dp[n][sum];
+    }
 }
