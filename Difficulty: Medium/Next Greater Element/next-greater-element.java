8d41b4025392b1dc6ead722d97cdeb55285536fc
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-25 19:59:55 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -3,16 +3,18 @@ class Solution {
         // code here
         Stack<Integer> st = new Stack<>();
         int n = arr.length;
-        int[] ans = new int[n];
-        ArrayList<Integer> list =  new ArrayList<Integer>();
+        int[] result = new int[n];
+        ArrayList<Integer> list = new ArrayList<Integer>();
         
-        for(int i = n-1; i >= 0; i--){
-            while(!st.isEmpty() && arr[i] >= arr[st.peek()]) st.pop();
-            if(st.isEmpty()) ans[i] = -1;
-            else ans[i] = arr[st.peek()];
-            st.push(i);
+        for(int i = n-1; i >=0; i--){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]) st.pop();
+            if(!st.isEmpty()) result[i] = arr[st.peek()];
+            else result[i] = -1;
+            
+            st.add(i);
         }
-        for(int i : ans) list.add(i);
+        for(int i : result) list.add(i);
+        
         return list;
     }
 }
