abead79716bb5f29ee2b9b7389ea709757060064
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-28 16:09:20 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,23 +1,19 @@
 class Solution {
+    int[][] dp;
     public int knapsack(int W, int val[], int wt[]) {
-        // code here
-        //base case
         int n = val.length;
-        int[][] dp = new int[n+1][W+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        int result = helper(W, val, wt, n, dp);
-        return result;
+        dp = new int[n+1][W+1];
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+        return helper(W, val, wt, n);
     }
     
-    public int helper(int W, int val[], int wt[], int n, int[][] dp){
-        if(n == 0 || W == 0) {
-            return 0;
-        }
+    public int helper(int W, int val[], int wt[], int n){
+        if(n == 0 || W == 0) return 0;
         if(dp[n][W] != -1) return dp[n][W];
-        else if(wt[n-1] <= W){
-             dp[n][W] = Math.max(val[n-1] + helper(W - wt[n-1], val, wt, n-1, dp), helper(W, val, wt, n-1, dp));
+        if(wt[n-1] <= W){
+            dp[n][W] = Math.max(val[n-1] + helper(W - wt[n-1], val, wt, n-1), helper(W, val, wt, n-1));
         }
-        else dp[n][W] = helper(W, val, wt, n-1, dp);
+        else dp[n][W] = helper(W, val, wt, n-1);
         return dp[n][W];
     }
 }
