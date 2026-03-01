8f99a562a46ede819ccf3183ff154acfe5b5c7be
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-28 19:51:03 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
@@ -1,32 +1,20 @@
 class Solution {
+    int[][] dp;
     public int count(int coins[], int sum) {
         int n = coins.length;
-        int[][] dp = new int[n+1][sum+1];
-       for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= sum; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(coins, sum, n, dp);
+        dp = new int[n+1][sum+1];
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+        return helper(coins, sum, n);
     }
     
-    public int helper(int coins[], int sum, int n, int[][] dp){
-        if(sum == 0){
-            return 1;
+    public int helper(int wt[], int W, int n){
+        if(W == 0) return 1;
+        if(n == 0 && W > 0) return 0;
+        if(dp[n][W] != -1) return dp[n][W];
+        if(wt[n-1] <= W){
+             dp[n][W] = helper(wt, W - wt[n-1], n) + helper(wt, W, n-1);
         }
-        if( n == 0){
-            return 0;
-        }
-        if(dp[n][sum] != -1){
-            return dp[n][sum];
-        }
-        //int count = 0;
-        if(coins[n-1] <= sum){
-            dp[n][sum] = helper(coins, sum - coins[n-1], n, dp) + helper(coins, sum, n-1, dp);
-        }
-        else{
-            dp[n][sum] = helper(coins, sum, n-1, dp);
-        }
-        return dp[n][sum];
+        else dp[n][W] = helper(wt, W, n-1);
+        return dp[n][W];
     }
 }
