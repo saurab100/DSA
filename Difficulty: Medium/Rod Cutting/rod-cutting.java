9838c9cf45e6025adcb7faae3706dbd934a6a136
Saurabh Saxena <saurabh.jan03@gmail.com>
2026-02-28 20:09:20 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -1,34 +1,22 @@
 class Solution {
+    int[][] dp;
     public int cutRod(int[] price) {
         // code here
         int n = price.length;
-        int[] wt = new int[n];
-        for(int i = 0; i < n; i++){
-            wt[i] = i+1;
-        }
-        int[][] dp = new int[n+1][n+1];
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= n; j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(price, wt, n, n, dp);
+        int rodLength = n;
+        dp = new int[n+1][rodLength+1];
+        for(int[] arr : dp) Arrays.fill(arr, -1);
+        return helper(price, rodLength, n);
     }
     
-    public int helper(int[] price, int[] wt, int cap, int n, int[][] dp){
-        if(cap == 0 || n == 0){
-            return 0;
+    public int helper(int val[], int rodLength, int n){
+        if(rodLength == 0 || n == 0) return 0;
+        
+        if(dp[n][rodLength] != -1) return dp[n][rodLength];
+        if(n <= rodLength){
+            dp[n][rodLength] =  Math.max(val[n-1] + helper(val, rodLength - n, n), helper(val, rodLength, n-1));
         }
-        if(dp[n][cap] != -1){
-            return dp[n][cap];
-        }
-        if(wt[n-1] <= cap){
-            dp[n][cap] =  Math.max(price[n-1] + helper(price, wt, cap - wt[n-1], n, dp),
-            helper(price, wt, cap, n-1, dp));
-        }
-        else{
-            dp[n][cap] =  helper(price, wt, cap, n-1, dp);
-        }
-        return dp[n][cap];
+        else dp[n][rodLength] =  helper(val, rodLength, n-1);
+        return dp[n][rodLength];
     }
 }
